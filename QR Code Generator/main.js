e960f69d150d4e48c4ccf0fc9a66553d01af38ff
bidheyakthapa <bidheyakthapa321@gamil.com>
2024-04-25 22:23:54 +0545

    Complete QR Code Generator

--- QR Code Generator/main.js
+++ QR Code Generator/main.js
@@ -3,7 +3,15 @@ let qrImg = document.getElementById("qrImg");
 let qrText = document.getElementById("qrText");
 
 function generateqr() {
-  qrImg.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrText.value;
+  if (qrText.value.length > 0) {
+    qrImg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+    imgBox.classList.add("show-img");
+  } else {
+    qrText.classList.add("error");
+    setTimeout(() => {
+      qrText.classList.remove("error");
+    }, 1000);
+  }
 }

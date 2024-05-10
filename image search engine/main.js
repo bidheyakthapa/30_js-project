24a7b4c55e6aa36a9c4f93fd70b67cf7df191259
bidheyakthapa <bidheyakthapa321@gamil.com>
2024-05-10 21:50:03 +0545

    Complet image search engine

--- image search engine/main.js
+++ image search engine/main.js
@@ -9,16 +9,48 @@ let keyword = "";
 let page = 1;
 
 async function searchImages() {
-  keyword = searchBox.Value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
   const response = await fetch(url);
   const data = await response.json();
 
-  console.log(data);
+  const results = data.results;
+
+  if (page == 1) {
+    searchResult.innerHTML = "";
+  }
+
+  if (results.length > 0) {
+    results.forEach((result) => {
+      const image = document.createElement("img");
+      image.src = result.urls.small;
+      const imageLink = document.createElement("a");
+      imageLink.href = result.links.html;
+      imageLink.target = "_blank";
+
+      imageLink.appendChild(image);
+      searchResult.appendChild(imageLink);
+    });
+    showMoreBtn.style.display = "block";
+  } else {
+    searchResult.innerHTML = "<p>No results found.</p>";
+    showMoreBtn.style.display = "none";
+  }
 }
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   page = 1;
   searchImages();
+});
+
+showMoreBtn.addEventListener("click", () => {
+  page++;
+  searchImages();
+});
+
+searchBox.addEventListener("keydown", (event) => {
+  if (event.key == "ENTER") {
+    searchImages();
+  }
 });
